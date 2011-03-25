f43a119b11f2973ef57af285e8848369a915a91e
c29m <c29.hunter@gmail.com>
2011-03-25 16:43:48 +0000

    Added the ability to generate a Javascript client side callback to the Javascript StreamPublishBuilder to enable further client side manipulation of the attachment description

--- FacebookExtensions/Markup/Javascript/StreamPublishAttachment.cs
+++ FacebookExtensions/Markup/Javascript/StreamPublishAttachment.cs
@@ -7,13 +7,22 @@
         public string Description { get; set; }
         public string Href { get; set; }
         public StreamPublishMediaItem Media { get; set; }
-
+        public string DescriptionClientCallbackFunction { get; set; }
+        public string DescriptionClientCallbackData { get; set; }
         public StreamPublishAttachment()
         {
             Name = string.Empty;
             Caption = string.Empty;
             Description = string.Empty;
             Href = string.Empty;
+            DescriptionClientCallbackData = string.Empty;
+            DescriptionClientCallbackFunction = string.Empty;
+        }
+
+        public StreamPublishAttachment(string descriptionClientCallbackFunction, string descriptionClientCallbackData):this()
+        {
+            DescriptionClientCallbackData = descriptionClientCallbackData;
+            DescriptionClientCallbackFunction = descriptionClientCallbackFunction;
         }
     }
 }
